4e3d7645357f213c63fd4f31b294a991e308e042
Your Name <you@example.com>
2019-12-04 18:02:30 -0500

    day 3 done! yay!

--- day3/part2.js
+++ day3/part2.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const rs = require('readline-sync');
 const _ = require('underscore');
-
 const calculateWirePositions = require('./CalculateWirePlaces.js');
 
+const intersections = [{"x":-477,"y":1650},{"x":-182,"y":2596},{"x":-10,"y":2665},{"x":46,"y":2665},{"x":368,"y":2857},{"x":368,"y":2905},{"x":156,"y":3222},{"x":-83,"y":3222},{"x":382,"y":3163},{"x":433,"y":2905},{"x":382,"y":2904},{"x":-10,"y":2904},{"x":-35,"y":3222},{"x":-705,"y":2356},{"x":-1007,"y":2145},{"x":-1007,"y":1749},{"x":-541,"y":1701},{"x":24,"y":1650},{"x":1327,"y":873},{"x":1488,"y":873},{"x":1859,"y":768},{"x":1859,"y":493},{"x":1327,"y":575},{"x":1674,"y":493},{"x":1869,"y":493},{"x":1869,"y":768},{"x":1869,"y":936},{"x":1327,"y":1222},{"x":896,"y":1407}].map(point => JSON.stringify(point));
 const wires = String(fs.readFileSync('input.txt')).split('\n');
 const wireOne = wires[0].split(',');
 const wireTwo = wires[1].split(',');
@@ -35,40 +35,4 @@ const wireOnePositions = calculateWirePositions(wireOne)
 const wireTwoPositions = calculateWirePositions(wireTwo)
 .map(point => JSON.stringify(point));
 
-console.log('positions calculated');
-
-const length = wireOnePositions.length;
-
-const intersections = wireOnePositions.filter(point1 => {
-  if(i % 1000 === 0) {
-    console.log(i);
-  }
-  i++
-  return wireTwoPositions.includes(point1)
-}).map(point => {
-  return JSON.parse(point);
-})
-// const intersections = wireOnePositions.filter(
-//   point1 => {
-//     if(i % 10 === 0) {
-//       console.log(`iteration ${i} of ${length}`)
-//     }
-//     i++
-//     return wireTwoPositions.filter(point2 => {
-//       _.isEqual(point1, point2);
-//     })
-//   }
-// );
-
-console.log('intersections calculated.');
-fs.writeFileSync('hello', JSON.stringify(intersections))
-console.log('written to file');
-let min = Infinity;
-for(intersection of intersections) {
-  const distance = manhattanDistance(intersection, {x: 0, y: 0});
-  if(distance < min) {
-    min = distance;
-  }
-}
-console.log('smallest manhattan:');
-console.log(min);
+console.log(wireOnePositions.indexOf(intersections[0]) + wireTwoPositions.indexOf(intersections[0]) + 2)
